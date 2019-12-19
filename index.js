93c1ceb0d9c7205c9f4d321c3ab07109ce573904
Teo <teotaneski2@gmail.com>
2019-12-19 16:25:56 +0100

    fetching the API

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hi there!');
+const fetchData = async () => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params : {
+			apikey : '6d8755d9',
+			s      : 'avengers'
+		}
+	});
+	console.log(response.data);
+};
+fetchData();
