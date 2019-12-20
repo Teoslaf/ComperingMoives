21b1ef11f4a8802c71e52b48fac5e7b36c4e472a
Teo <teotaneski2@gmail.com>
2019-12-20 19:33:32 +0100

    Added the event listener

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const fetchData = async () => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params : {
 			apikey : '6d8755d9',
-			s      : 'avengers'
+			s      : searchTerm
 		}
 	});
 	console.log(response.data);
 };
-fetchData();
+
+const input = document.querySelector('input');
+
+let timeoutId;
+
+const onInput = (event) => {
+	setTimeout(() => {
+		fetchData(event.target.value);
+	}, 1000);
+};
+
+input.addEventListener('input', onInput);
