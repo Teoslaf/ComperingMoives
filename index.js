e4aa13e9dfea04d07648e520daf9e685aa6d1241
Teo <teotaneski2@gmail.com>
2019-12-23 16:00:34 +0100

    appending some data

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params : {
-			apikey : '6d8755d9',
+			apikey : 'd9835cc5',
 			s      : searchTerm
 		}
 	});
-	console.log(response.data);
+
+	return response.data.Search;
 };
 
 const input = document.querySelector('input');
-const debounce = (func, delay) => {
-	let timeoutId;
-	return (...args) => {
-		if (timeoutId) {
-			clearTimeout(timeoutId);
-		}
-		timeoutId = setTimeout(() => {
-			func.apply(null, args);
-		}, delay);
-	};
+
+const onInput = async (event) => {
+	const movies = await fetchData(event.target.value);
+
+	for (let movie of movies) {
+		const div = document.createElement('div');
+
+		div.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title}</h1>
+    `;
+
+		document.querySelector('#target').appendChild(div);
+	}
 };
-
-const onInput = debounce((event) => {
-	fetchData(event.target.value);
-});
-
 input.addEventListener('input', debounce(onInput, 500));
